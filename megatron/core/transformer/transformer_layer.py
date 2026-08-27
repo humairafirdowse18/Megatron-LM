0c7e90518be4e07f72b370ad9808d45b8e758d1d
humairafirdowse18 <humairafirdo@nvidia.com>
2026-08-26 18:24:31 -0700

    reverted logging
    Signed-off-by: humairafirdowse18 <humairafirdo@nvidia.com>

--- megatron/core/transformer/transformer_layer.py
+++ megatron/core/transformer/transformer_layer.py
@@ -28,11 +28,7 @@ from megatron.core.enums import Fp8Recipe
 from megatron.core.inference.utils import InferenceMode
 from megatron.core.packed_seq_params import PackedSeqParams
 from megatron.core.process_groups_config import ProcessGroupCollection
-from megatron.core.transformer.cuda_graphs import (
-    is_graph_capturing,
-    record_packed_cg_fallback,
-    record_packed_cg_replay,
-)
+from megatron.core.transformer.cuda_graphs import is_graph_capturing
 from megatron.core.transformer.enums import CudaGraphModule, InferenceCudaGraphScope, LayerType
 from megatron.core.transformer.identity_op import IdentityFuncOp, IdentityOp
 from megatron.core.transformer.mlp import MLP
@@ -1584,7 +1580,6 @@ class TransformerLayer(GraphableMegatronModule, BaseTransformerLayer, TwoStageAt
             bucket_max = self._cuda_graph_psp_buffers['cu_seqlens_q'].shape[0]  # max_seqs + 1
             if psp.cu_seqlens_q.shape[0] > bucket_max:
                 # Actual N_docs exceeds bucket -> fall back to non-CG forward.
-                record_packed_cg_fallback()
                 return self.forward(*args, **kwargs)
 
         context = None
@@ -1655,7 +1650,6 @@ class TransformerLayer(GraphableMegatronModule, BaseTransformerLayer, TwoStageAt
         if self.config.delay_offload_until_cuda_graph:
             self.off_interface.enter_replay()
 
-        record_packed_cg_replay()
         try:
             return self._te_cuda_graph_replay_impl(args, kwargs, context)
         finally:
